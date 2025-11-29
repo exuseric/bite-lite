977447b9494daa1bc3a9afc0a709a84d3fefab1e
exuseric <ericbro29@gmail.com>
2025-11-29 20:03:26 +0300

    update: add api routes to mock server data fetching

--- store/product-store.ts
+++ store/product-store.ts
@@ -1,27 +1,42 @@
+import categoriesData from "@/data/categories.json";
 import { create } from "zustand";
 import { ProductCategory, ProductItem } from "../types/product-types";
-import productData from "@/data/products.json";
-import categoriesData from "@/data/categories.json";
 
 type StoreType = {
   products: ProductItem[];
   categories: ProductCategory[];
+  searchTerm: string;
+  searchResults: ProductItem[];
   getProducts: () => Promise<void>;
   getCategories: () => Promise<void>;
+  setSearchTerm: (term: string) => void;
 };
 
-const useProductStore = create<StoreType>((set) => ({
+const useProductStore = create<StoreType>((set, get) => ({
   products: [],
   categories: [],
+  searchTerm: "",
+  searchResults: [],
   getProducts: async () => {
-    const response = await fetch("/api/products");
-    const data = await response.json();
-    console.log({ data });
+    const res = await fetch("/api/products");
+    const data = await res.json();
     set({ products: data });
   },
   getCategories: async () => {
-    await new Promise((r) => setTimeout(r, 300));
-    set({ categories: categoriesData.categories });
+    const res = await fetch("/api/categories");
+    const data = await res.json();
+    set({ categories: data });
+  },
+  setSearchTerm: (term: string) => {
+    const products = get().products;
+
+    const results =
+      term.trim().length === 0 ? [] : products.filter((p) => p.name.toLowerCase().includes(term.toLowerCase()));
+
+    set({
+      searchTerm: term,
+      searchResults: results,
+    });
   },
 }));
 
