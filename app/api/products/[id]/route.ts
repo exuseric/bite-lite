435fcd9e8032aa6596509808831f3d303690c5b1
exuseric <ericbro29@gmail.com>
2025-11-30 18:15:20 +0300

    refactor: Fix build type errors - remove server-side product API - route and enhance fetch-product utility with improved types and error handling - adjust README to reflect updated state management details and mocked API design

--- app/api/products/[id]/route.ts
+++
@@ -1,8 +0,0 @@
-import data from "@/data/products.json";
-
-export async function GET({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;
-  const product = data.products.find((product) => product.id === id);
-  await new Promise((r) => setTimeout(r, 1000));
-  return Response.json(product);
-}
